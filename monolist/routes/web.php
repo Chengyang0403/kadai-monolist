429c0f536cbede0158bc0454d54f40528f3df86e
cheng.yang <cheng.yang@rakuten.com>
2018-06-21 05:06:07 +0000

    user registration

--- monolist/routes/web.php
+++ monolist/routes/web.php
@@ -2,6 +2,10 @@
 
 Route::get('/', 'WelcomeController@index');
 
+// User registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 Route::get('/', function () {
     return view('welcome');
 });
